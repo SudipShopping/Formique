66a2c1f7ce17f5351eb619dafdb940e18d957ee2
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:13:17 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,1 +1,20 @@
+// routes/productRoutes.js
+const express = require('express');
+const {
+  createProduct, getProducts, getProductById,
+  updateProduct, deleteProduct, approveProduct
+} = require('../controllers/productController');
+const { verifyToken, isSeller, isAdmin } = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/upload');
+const router = express.Router();
+
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.post('/', verifyToken, isSeller, upload.array('images', 5), createProduct);
+router.put('/:id', verifyToken, isSeller, upload.array('images', 5), updateProduct);
+router.delete('/:id', verifyToken, isSeller, deleteProduct);
+// Admin can approve products
+router.patch('/:id/approve', verifyToken, isAdmin, approveProduct);
+
+module.exports = router;
 
