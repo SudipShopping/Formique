913778cfc9415b937d8b1af084491693bf243391
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:13:37 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,1 +1,17 @@
+// routes/orderRoutes.js
+const express = require('express');
+const {
+  createOrder, verifyPayment, getMyOrders,
+  getAllOrders, updateOrderStatus
+} = require('../controllers/orderController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.post('/', verifyToken, createOrder);
+router.post('/verify', verifyToken, verifyPayment);
+router.get('/myorders', verifyToken, getMyOrders);
+router.get('/', verifyToken, isAdmin, getAllOrders);
+router.patch('/:id/status', verifyToken, isAdmin, updateOrderStatus);
+
+module.exports = router;
 
