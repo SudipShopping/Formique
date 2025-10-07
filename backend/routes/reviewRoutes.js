d2f0594ae855862bd7b420be219cb093fc1c8f67
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:15:15 +0530

    Update reviewRoutes.js

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,1 +1,10 @@
+// routes/reviewRoutes.js
+const express = require('express');
+const { addReview } = require('../controllers/reviewController');
+const { verifyToken } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.post('/', verifyToken, addReview);
+
+module.exports = router;
 
