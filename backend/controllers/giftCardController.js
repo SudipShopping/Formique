7e4a7ad1a39cfe8bbb5573b3daf3c1f4ee2b27cb
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:10:55 +0530

    Update giftCardController.js

--- backend/controllers/giftCardController.js
+++ backend/controllers/giftCardController.js
@@ -1,1 +1,29 @@
+// controllers/giftCardController.js
+const GiftCard = require('../models/GiftCard');
+const User     = require('../models/User');
+
+exports.createGiftCard = async (req, res, next) => {
+  try {
+    const { code, amount } = req.body;
+    const card = await GiftCard.create({ code, amount });
+    res.status(201).json(card);
+  } catch (err) { next(err); }
+};
+
+exports.redeemGiftCard = async (req, res, next) => {
+  try {
+    const { code } = req.body;
+    const card = await GiftCard.findOne({ code });
+    if (!card || card.isRedeemed) {
+      return res.status(400).json({ message: 'Invalid gift card' });
+    }
+    const user = await User.findById(req.user.id);
+    user.fmcoins += card.amount;
+    await user.save();
+    card.isRedeemed = true;
+    card.redeemedBy  = req.user.id;
+    await card.save();
+    res.json({ message: 'Gift card redeemed', fmcoins: user.fmcoins });
+  } catch (err) { next(err); }
+};
 
