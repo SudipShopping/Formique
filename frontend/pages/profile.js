97ba840be020d25182d64f55dfc98099ee4478ab
SudipShopping <sudipusa9@gmail.com>
2025-10-07 15:33:53 +0530

    Update profile.js

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,1 +1,18 @@
+import { useSelector } from 'react-redux';
+
+export default function Profile() {
+  const user = useSelector((state) => state.auth.user);
+
+  if (!user) {
+    return <div>Please login to view your profile.</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Profile</h1>
+      <p><span className="font-semibold">Phone:</span> {user.phone}</p>
+      {/* You can add more profile fields and a form to update profile */}
+    </div>
+  );
+}
 
