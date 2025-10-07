075505dc1de3a58ca1de11d82d92ed87ddbd3675
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:13:53 +0530

    Update cartRoutes.js

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,1 +1,13 @@
+// routes/cartRoutes.js
+const express = require('express');
+const { getCart, addToCart, removeFromCart, clearCart } = require('../controllers/cartController');
+const { verifyToken } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.get('/', verifyToken, getCart);
+router.post('/add', verifyToken, addToCart);
+router.delete('/remove/:productId', verifyToken, removeFromCart);
+router.delete('/clear', verifyToken, clearCart);
+
+module.exports = router;
 
