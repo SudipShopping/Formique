40707064d353f6eabcdf7bbc395b00fbf899e03e
SudipShopping <sudipusa9@gmail.com>
2025-10-07 15:43:50 +0530

    Update index.js

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,1 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import cartReducer from './slices/cartSlice';
+import wishlistReducer from './slices/wishlistSlice';
+
+export default configureStore({
+  reducer: {
+    auth: authReducer,
+    cart: cartReducer,
+    wishlist: wishlistReducer,
+  },
+});
 
