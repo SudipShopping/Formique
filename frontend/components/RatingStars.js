041e43f35827b127a01df251ef306c5f7d76150b
SudipShopping <sudipusa9@gmail.com>
2025-10-07 15:42:20 +0530

    Update RatingStars.js

--- frontend/components/RatingStars.js
+++ frontend/components/RatingStars.js
@@ -1,1 +1,11 @@
-
+export default function RatingStars({ rating }) {
+  return (
+    <div className="flex">
+      {[...Array(5)].map((_, i) => (
+        <span key={i} className={i < rating ? 'text-yellow-500' : 'text-gray-300'}>
+          ★
+        </span>
+      ))}
+    </div>
+  );
+}
