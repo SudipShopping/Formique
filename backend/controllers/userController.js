2ff8b70c7e05bd70cf408e548109366b8aceae48
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:08:04 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,25 @@
+// controllers/userController.js
+const User = require('../models/User');
+
+exports.getProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) { next(err); }
+};
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    const updates = req.body;
+    const user = await User.findByIdAndUpdate(req.user.id, updates, { new: true }).select('-password');
+    res.json(user);
+  } catch (err) { next(err); }
+};
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (err) { next(err); }
+};
 
