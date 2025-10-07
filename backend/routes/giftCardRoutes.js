34429a47d3df7b72943aee6768a7f9e8890cb2c1
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:15:54 +0530

    Update giftCardRoutes.js

--- backend/routes/giftCardRoutes.js
+++ backend/routes/giftCardRoutes.js
@@ -1,1 +1,11 @@
+// routes/giftCardRoutes.js
+const express = require('express');
+const { createGiftCard, redeemGiftCard } = require('../controllers/giftCardController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.post('/', verifyToken, isAdmin, createGiftCard);
+router.post('/redeem', verifyToken, redeemGiftCard);
+
+module.exports = router;
 
