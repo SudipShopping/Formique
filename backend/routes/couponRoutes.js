125bae962bd08899531f87ebcc237fd8a9f6378e
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:16:17 +0530

    Update couponRoutes.js

--- backend/routes/couponRoutes.js
+++ backend/routes/couponRoutes.js
@@ -1,1 +1,11 @@
+// routes/couponRoutes.js
+const express = require('express');
+const { createCoupon, getCoupon } = require('../controllers/couponController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.post('/', verifyToken, isAdmin, createCoupon);
+router.get('/:code', verifyToken, getCoupon);
+
+module.exports = router;
 
