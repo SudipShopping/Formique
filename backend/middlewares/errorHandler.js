b304c08079c93b7e0b06d4fcedf7925d93ca09c9
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:17:44 +0530

    Update errorHandler.js

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,1 +1,7 @@
+// middlewares/errorHandler.js
+// Error-handling middleware must have 4 arguments (err, req, res, next):contentReference[oaicite:10]{index=10}.
+module.exports = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: err.message });
+};
 
