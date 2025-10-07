6c184301a6528d8e70ce3c629e751b08929f995d
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:18:01 +0530

    Update upload.js

--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -1,1 +1,11 @@
+// middlewares/upload.js
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../config/cloudinary');
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: { folder: 'formique/products', allowed_formats: ['jpg', 'png'] },
+});
+const upload = multer({ storage });
+module.exports = upload;
 
