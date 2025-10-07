746a3d7825fa98f287c65f6f0db51d7dd0a74ae7
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:11:44 +0530

    Update adminController.js

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,1 +1,14 @@
+// controllers/adminController.js
+const Product = require('../models/Product');
+const Order   = require('../models/Order');
+const User    = require('../models/User');
+
+exports.analytics = async (req, res, next) => {
+  try {
+    const productCount = await Product.countDocuments();
+    const userCount    = await User.countDocuments();
+    const orderCount   = await Order.countDocuments();
+    res.json({ productCount, userCount, orderCount });
+  } catch (err) { next(err); }
+};
 
