78894c04e247783ed66254efc30231d491677ca7
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:05:00 +0530

    Update Cart.js

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,1 +1,15 @@
+// models/Cart.js
+const mongoose = require('mongoose');
+const cartSchema = new mongoose.Schema({
+  user:      { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items:     [
+    {
+      product:  { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+      quantity: { type: Number, required: true, min: 1, default: 1 },
+      price:    { type: Number },
+    },
+  ],
+  totalBill: { type: Number, default: 0 },
+}, { timestamps: true });
+module.exports = mongoose.model('Cart', cartSchema);
 
