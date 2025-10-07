11f1819be3bf7dc6c61314ab944e32f40e3d5d55
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:04:15 +0530

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,1 +1,14 @@
+// models/Product.js
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  seller:     { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  name:       { type: String, required: true },
+  description:{ type: String },
+  category:   { type: String },
+  price:      { type: Number, required: true },
+  images:     [{ type: String }], // store URLs from Cloudinary
+  stock:      { type: Number, default: 0 },
+  isApproved: { type: Boolean, default: false }, // for admin approval
+}, { timestamps: true });
+module.exports = mongoose.model('Product', productSchema);
 
