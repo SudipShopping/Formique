1b6287f16da656a7f6cf771bd60631ab76d69d14
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:12:36 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,10 @@
+// routes/authRoutes.js
+const express = require('express');
+const { sendOTP, verifyOTP } = require('../controllers/authController');
+const router = express.Router();
+
+router.post('/send-otp', sendOTP);
+router.post('/verify-otp', verifyOTP);
+
+module.exports = router;
 
