b7c0abd9c6ebe7ebde9180fbc2e3c9c291e8631f
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:12:54 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,12 @@
+// routes/userRoutes.js
+const express = require('express');
+const { getProfile, updateProfile, getAllUsers } = require('../controllers/userController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.get('/me', verifyToken, getProfile);
+router.put('/me', verifyToken, updateProfile);
+router.get('/', verifyToken, isAdmin, getAllUsers);
+
+module.exports = router;
 
