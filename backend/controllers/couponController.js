bbe1696380cd8f347dccd15e2aab225887a1b411
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:11:23 +0530

    Update couponController.js

--- backend/controllers/couponController.js
+++ backend/controllers/couponController.js
@@ -1,1 +1,24 @@
+// controllers/couponController.js
+const Coupon = require('../models/Coupon');
+
+exports.createCoupon = async (req, res, next) => {
+  try {
+    const { code, discountPercent, expireDate } = req.body;
+    const coupon = await Coupon.create({ code, discountPercent, expireDate });
+    res.status(201).json(coupon);
+  } catch (err) { next(err); }
+};
+
+exports.getCoupon = async (req, res, next) => {
+  try {
+    const { code } = req.params;
+    const coupon = await Coupon.findOne({
+      code,
+      isActive: true,
+      expireDate: { $gt: new Date() },
+    });
+    if (!coupon) return res.status(404).json({ message: 'Coupon invalid or expired' });
+    res.json(coupon);
+  } catch (err) { next(err); }
+};
 
