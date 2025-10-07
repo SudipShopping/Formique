c02305aeff459016213eb8e3524226d98a6a84de
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:10:31 +0530

    Update reviewController.js

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,1 +1,16 @@
+// controllers/reviewController.js
+const Review = require('../models/Review');
+
+exports.addReview = async (req, res, next) => {
+  try {
+    const { productId, rating, comment } = req.body;
+    const review = await Review.create({
+      user: req.user.id,
+      product: productId,
+      rating,
+      comment,
+    });
+    res.status(201).json(review);
+  } catch (err) { next(err); }
+};
 
