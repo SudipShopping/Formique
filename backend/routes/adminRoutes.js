b21913b144684a44640417aec997ba94e847865b
SudipShopping <sudipusa9@gmail.com>
2025-10-07 17:16:48 +0530

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,1 +1,10 @@
+// routes/adminRoutes.js
+const express = require('express');
+const { analytics } = require('../controllers/adminController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.get('/analytics', verifyToken, isAdmin, analytics);
+
+module.exports = router;
 
